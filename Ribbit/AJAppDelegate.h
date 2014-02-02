0f4b9d02fb4df972f59746629e3ecec01322ffd3
Alberto Jauregui <bastertwo@gmail.com>
2014-02-01 22:20:30 -0700

    Initial Commit

---
+++ Ribbit/AJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AJAppDelegate.h
+//  Ribbit
+//
+//  Created by Alberto Jauregui on 2/1/14.
+//  Copyright (c) 2014 Alberto Jauregui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ribbit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ribbit
+//
+//  Created by Alberto Jauregui on 2/1/14.
+//  Copyright (c) 2014 Alberto Jauregui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AJAppDelegate class]));
+    }
+}
